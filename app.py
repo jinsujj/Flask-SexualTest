3a24b8b836c313562bb5edf40733d59c94e408f9
Jinsu <wlstncjs1234@naver.com>
2020-08-23 19:51:55 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb://test:test@54.180.103.20',27017)
+db = client.user  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
+
+
+@app.route('/')
+def home():
+    return render_template('intro.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/loading')
+def loading():
+    return render_template('loading.html')
+
+@app.route('/result/ISTJ')
+def result_ISTG():
+    return render_template('ISTJ.html')
+
+@app.route('/result/ISFJ')
+def result_ISFJ():
+    return render_template('ISFJ.html')
+
+@app.route('/result/INTJ')
+def result_INTJ():
+    return render_template('INTJ.html')
+
+@app.route('/result/INFJ')
+def result_INFJ():
+    return render_template('INFJ.html')
+
+@app.route('/result/ESTJ')
+def result_ESTJ():
+    return render_template('ESTJ.html')
+
+@app.route('/result/ENTP')
+def result_ENTP():
+    return render_template('ENTP.html')
+
+
+@app.route('/api/result', methods=['POST'])
+def get_result():
+    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    array = request.form.getlist('res[]')
+
+    print(len(array))
+    cnt_a = 0
+    cnt_b = 0
+    result = ""
+
+    # 구체적인 로직으로 변경해야해
+    for index, value in enumerate(array):
+        if value == 'A':
+            cnt_a += 1
+        elif value == 'B':
+            cnt_b += 1
+
+    print(cnt_a, cnt_b)
+
+    if cnt_a <= 2:
+        result = 'ISTJ'
+    elif cnt_a <= 4:
+        result = 'ISFJ'
+    elif cnt_a <= 6:
+        result = 'INFJ'
+    elif cnt_a <= 8:
+        result = 'INTJ'
+    elif cnt_a <= 10:
+        result = 'ESTJ'
+    elif cnt_a <= 12:
+        result = 'ENTP'
+
+    return jsonify({'result': result})
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=5000, debug=True)
---
+++ static/css3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>반응형 웹 - 미디어 쿼리</title>
+<link rel="stylesheet" type="text/css" href="./mystyle.css" />
+<!--[if lte IE 8]>
+<script type="text/javascript" src="./respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+<h3>반응형 웹 - 미디어 쿼리</h3>
+<pre style="border:solid 1px black;overflow:auto;">
+&lt;link rel="stylesheet" type="text/css" href="./mystyle.css" /&gt;
+&lt;!--[if lte IE 8]&gt;&gt;
+&lt;script type="text/javascript" src="./respond.min.js"&gt;&lt;/script&gt;
+&lt;![endif]--&gt;
+</pre>
+</body>
+</html>

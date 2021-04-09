6145105451a2d79c61945b729f66eaf4b20e4823
Jinsu <wlstncjs1234@naver.com>
2021-04-10 00:18:38 +0900

    update

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import datetime
 from flask import Flask, render_template, jsonify, request, send_file
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 client = MongoClient('mongodb://jinsu:wlstncjs1!@54.180.103.20', 27017)
-db = client.log  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
+db = client.log
 
 @app.route('/sitemap.xml')
 def sitemap():
